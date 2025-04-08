399f59b90033b0221a3e8a2f3f55c2d913daa6ca
HenriqueBiazon <henriquebbff@gmail.com>
2025-04-08 18:04:03 -0300

    Update jogo.py
    mudanca geral, agora o codigo roda certinho

--- jogo.py
+++ jogo.py
@@ -3,97 +3,129 @@ import random
 # Jogo de adivinhação
 
 # Tela inicial
+print('------------------------------------------------------------------------------------------------------')
+print('------------------------------------- Seja bem-vindo ao jogo!!!! -------------------------------------')
+print('------------------------------------------------------------------------------------------------------')
+print('Você deverá achar o número de 4 dígitos. Para isso terá 10 tentativas (o número está entre 1000 e 9999)')
+print('A partir da 5ª tentativa você receberá dicas.')
 
-print('-----------------------------------------------------------------------------------------------------')
-print('--------------------------------------seja bem vindo ao jogo!!!--------------------------------------')
-print('-----------------------------------------------------------------------------------------------------')
-print('Você deverá achar o número de 4 casas. para isso terá 10 tentativas (o número está entre 1000 e 9999)')
-print('À partir da 5a. tentativa você recebera dicas')
 
 
 
 # Começo do jogo
+comecar = int(input('Se você quer começar, digite 1: '))
 
-comecar = int(input('Se você quer começar digite 1: '))
-print('O jogo começou, se prepare')
+
+
 
 if comecar == 1:
-    numero = random.randint(1000,9999)   # Gerar numero aleatorio
-    contagem_tentativa = 0   #  Contador de tentativas
+    print('O jogo começou, se prepare!')
+    numero = random.randint(1000, 9999)   # Gerar número aleatório
+    contagem_tentativa = 0
+    acertou = False
+
+    
+    
+    
+    
     while contagem_tentativa < 10:
-        tentativa = int(input(f'tentativa {contagem_tentativa + 1} - Digite sua tentativa para acertar o número: '))
+        tentativa = int(input(f'Tentativa {contagem_tentativa + 1} - Digite sua tentativa para acertar o número: '))
+
+       
+       
+       
+        # Verificar se a tentativa é válida (tem 4 dígitos)
+        if tentativa < 1000 or tentativa > 9999:
+            print('Por favor, digite um número entre 1000 e 9999.')
+            continue  
         contagem_tentativa += 1
 
+        
+        
+        
+        
+        # Separar dígitos do número e da tentativa
+        n1 = numero // 1000
+        n2 = (numero % 1000) // 100
+        n3 = (numero % 100) // 10
+        n4 = numero % 10
 
-        #verificar se a tentativa e valida (tem 4 digitos)
+        t1 = tentativa // 1000
+        t2 = (tentativa % 1000) // 100
+        t3 = (tentativa % 100) // 10
+        t4 = tentativa % 10
 
-        if tentativa < 1000 or tentativa > 9999: 
-            print('Por favor, digite um número entre 1000 e 9999')
-            continue
-
-
-    
-
-        # Converter os números em lista de dígitos para facilitar a comparação
-
-        numero_secreto = [int(d) for d in str(numero)]
-        tentativa_lista = [int(d) for d in str(tentativa)]
-
-
-
-
-        #Mostra os acertos
-
-        resultado = ''
-        for i in range(4):
-            if tentativa_lista[i] == numero_secreto[i]:
-               resultado += str(tentativa_lista[i])
-            else:
-               resultado += '-'
-            print(f'Numero: {resultado}')
-    
-
-
-    
-    
-         # Dar dica a partir da 5a. tentativa
-
-        if tentativa >= 5:
-           posicao_dica = random.randint(0,4) #para escolher uma posicao aleatroria
-           dica = numero_secreto[posicao_dica]
-    
-
-
-
-        # Escolher qual dica dar
-
-        qual_dica = random.choice(['maior5', 'par_impar'])
-
-        if qual_dica == 'maior5':
-            if dica > 5:
-               print(f'Dica: o numero na posicao {posicao_dica} e maior que 5')
-            else:
-                print(f'Dica: o numero na posicao {posicao_dica} e menor ou igual a 5')
-    
+       
+       
+       
+        # Mostrar os acertos
+        print("Número: ", end='')
+        if t1 == n1:
+            print(t1, end='')
         else:
-            if dica % 2 == 0:
-               print(f'Dica: o numero na posicao {posicao_dica} e par')
-            else:
-               print(f'Dica: o numero na posicao {posicao_dica} e impar')
-    
-    
+            print('-', end='')
 
+        if t2 == n2:
+            print(t2, end='')
+        else:
+            print('-', end='')
 
-        #ver se o jogador acertou a dica
+        if t3 == n3:
+            print(t3, end='')
+        else:
+            print('-', end='')
 
+        if t4 == n4:
+            print(t4)
+        else:
+            print('-')
+
+       
+       
+       
+        # Verificar se acertou
         if tentativa == numero:
-           print(f'Voce acertou, o numero e: {numero} em {tentativa} tentativas')
+            print(f'Parabéns! Você acertou o número {numero} em {contagem_tentativa} tentativas!')
+            acertou = True
+            break
+
+       
+       
+       
+        # Dar dica a partir da 5ª tentativa
+        if contagem_tentativa >= 5:
+            posicao = random.randint(0, 3)
+
+            if posicao == 0:
+                digito = n1
+            elif posicao == 1:
+                digito = n2
+            elif posicao == 2:
+                digito = n3
+            else:
+                digito = n4
+
+            qual_dica = random.randint(0, 1)
+
+            if qual_dica == 0:
+                if digito > 5:
+                    print(f'Dica: o número na posição {posicao + 1} é maior que 5.')
+                else:
+                    print(f'Dica: o número na posição {posicao + 1} é menor ou igual a 5.')
+            else:
+                if digito % 2 == 0:
+                    print(f'Dica: o número na posição {posicao + 1} é par.')
+                else:
+                    print(f'Dica: o número na posição {posicao + 1} é ímpar.')
+
     
-        else:
-            print(f'Voce nao conseguiu acertar, o numero era: {numero}')
+    
+    
+    if not acertou:
+        print(f'Fim de jogo! O número correto era {numero}.')
 
 
-     # Caso nao iniciar o jogo
 
-    else:
-       print('Voce nao iniciou o jogo')
+
+else:
+    print('Você não iniciou o jogo.')
